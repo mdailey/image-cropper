019bc72e047f28054858a650c5e2879e0faa728e
Suwanna Xanthavanij <tama_opps@hotmail.com>
2015-06-22 10:58:53 +0700

    fix script for building jenkins

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,16 +2,11 @@
 
 # Run Rails application tests
 cd image_cropper
-ruby -v
-rails -v
-gem -v
-#gem install bundler
-echo 'finish runnint image cropper'
-#bundle install --without production
-#mkdir -p features/reports
-#bundle exec rake db:reset || exit 2
-#bundle exec rake db:create:all || exit 3
-#bundle exec rake db:migrate || exit 4
-#bundle exec rake db:seed || exit 5
-#RAILS_ENV=test bundle exec rake ci:setup:minitest test
-#RAILS_ENV=test xvfb-run -a bundle exec rake cucumber:ci
+bundle install --without production
+mkdir -p features/reports
+bundle exec rake db:reset || exit 2
+bundle exec rake db:create:all || exit 3
+bundle exec rake db:migrate || exit 4
+bundle exec rake db:seed || exit 5
+RAILS_ENV=test bundle exec rake ci:setup:minitest test
+RAILS_ENV=test xvfb-run -a bundle exec rake cucumber:ci
