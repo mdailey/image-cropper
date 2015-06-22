7852e343d5a4c378903bbbd51ecf7e33e89e1324
Suwanna Xanthavanij <tama_opps@hotmail.com>
2015-06-22 15:29:11 +0700

    fix script for running jenkins

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,5 +8,5 @@ bundle exec rake db:reset || exit 2
 bundle exec rake db:create:all || exit 3
 bundle exec rake db:migrate || exit 4
 bundle exec rake db:seed || exit 5
-RAILS_ENV=test bundle exec rake ci:setup:minitest test
-RAILS_ENV=test xvfb-run -a bundle exec rake cucumber:ci
+RAILS_ENV=test bundle exec rake test
+RAILS_ENV=test xvfb-run -a bundle exec rake cucumber
