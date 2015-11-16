a81d7af3d4c5c8f8bdbc6b805341f442336c2e54
Suwanna Xanthavanij <tama_opps@hotmail.com>
2015-11-16 09:26:56 +0700

    update staging

--- image_cropper/config/deploy/staging.rb
+++ image_cropper/config/deploy/staging.rb
@@ -1,61 +1,78 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
 
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
-# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
-# server 'db.example.com', user: 'deploy', roles: %w{db}
+set :scm, :git
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call if deploying?
 
+server 'cropper.pineapplevisionsystems.com',
+  user: 'deploy',
+  roles: %w{web app db},
+  ssh_options: {
+    forward_agent: true,
+    auth_methods: %w(publickey)
+  }
 
+namespace :deploy do
 
-# role-based syntax
-# ==================
+  namespace :processors do
 
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any  hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
+    desc "Update git repo on image cropper"
+    task :pull_repo do
+      on roles([:app]) do
+        within "#{current_path}" do
+          with rails_env: :production do
+            puts "Branch is #{fetch(:branch)}"
+            rake "processors:pull_repo[#{fetch(:branch)}]"
+          end
+        end
+      end
+    end
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+  end
 
+  namespace :db do
 
+    desc "Create Production Database"
+    task :create do
+      on roles([:db]) do
+        within "#{current_path}" do
+          with rails_env: :production do
+            rake "db:create"
+          end
+        end
+      end
+    end
 
-# Configuration
-# =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/
-# Feel free to add new variables to customise your setup.
+    desc "Migrate Production Database"
+    task :migrate do
+      on roles([:db]) do
+        within "#{current_path}" do
+          with rails_env: :production do
+            rake "db:migrate"
+          end
+        end
+      end
+    end
 
+    desc "Seed Production Database"
+    task :seed do
+      on roles([:db]) do
+        within "#{current_path}" do
+          with rails_env: :production do
+            rake "db:seed"
+          end
+        end
+      end
+    end
 
+    desc "Drop Production Database"
+    task :drop do
+      on roles([:db]) do
+        within "#{current_path}" do
+          with rails_env: :production do
+            rake "db:drop"
+          end
+        end
+      end
+    end
 
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+  end
+end
