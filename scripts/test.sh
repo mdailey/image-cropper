28e4d1aeed5fda9ab30abe5e6e563715be74b99f
Matthew Dailey <dailey.matthew@gmail.com>
2017-08-05 06:59:52 +0700

    Get CI reporting going

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,5 +8,6 @@ bundle exec rake db:reset || exit 2
 bundle exec rake db:create:all || exit 3
 bundle exec rake db:migrate || exit 4
 bundle exec rake db:seed || exit 5
-RAILS_ENV=test xvfb-run -a bundle exec rake cucumber
 RAILS_ENV=test bundle exec rake test
+RAILS_ENV=test xvfb-run -a bundle exec cucumber --profile default --format junit --out features/reports/ --format json -o features/reports/FEATURES.json
+
