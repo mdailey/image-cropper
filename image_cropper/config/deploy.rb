6837333202aff3e7295319fb3e0072dce9bb4bd4
Matthew Dailey <dailey.matthew@gmail.com>
2017-08-07 04:08:55 +0700

    Capistrano config

--- image_cropper/config/deploy.rb
+++ image_cropper/config/deploy.rb
@@ -1,13 +1,12 @@
 
-# config valid only for current version of Capistrano
-#lock '3.4.0'
-lock '3.2.1'
+lock '3.6.1'
 
 set :application, 'image_cropper'
 set :repo_url, 'git@vgl-ait.org:ImageCropper'
+set :scm, :git
 set :repo_tree, 'image_cropper'
 set :deploy_to, '/home/deploy/image_cropper'
-set :rbenv_ruby, '2.1.5'
+set :rbenv_ruby, '2.3.1'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :keep_releases, 5
