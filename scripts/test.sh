0b7b32367ffc6772cab629606cc5009fea16c62e
Matthew Dailey <dailey.matthew@gmail.com>
2017-08-06 07:05:19 +0700

    Fix test script to clean test database

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-# Run Rails application tests
+set -e
+
+# Prepare application for test
 cd image_cropper
 bundle install --without production
 mkdir -p features/reports
-bundle exec rake db:reset || exit 2
-bundle exec rake db:create:all || exit 3
-bundle exec rake db:migrate || exit 4
-bundle exec rake db:seed || exit 5
+bundle exec rake db:reset
+bundle exec rake db:create:all
+bundle exec rake db:migrate
+bundle exec rake db:seed
+
+# Clean test database
+USER="ImageCropper"
+PASS="ImageCropper"
+TESTDB="image_cropper_test"
+CMD="DELETE FROM roles;"
+mysql $TESTDB -u $USER --password=${PASS} -e "$CMD"
+
+# Run tests
 RAILS_ENV=test bundle exec rake test
 RAILS_ENV=test xvfb-run -a bundle exec cucumber --profile default --format junit --out features/reports/ --format json -o features/reports/FEATURES.json
 
