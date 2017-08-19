fd44edf00b989a7f892684524c391f117548ed19
Matthew Dailey <dailey.matthew@gmail.com>
2017-08-19 07:05:34 +0700

    Reset driver to poltergeist after a selenium run

--- image_cropper/features/support/env.rb
+++ image_cropper/features/support/env.rb
@@ -75,3 +75,20 @@ Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, options: options)
 end
 
+
+Before do
+  DatabaseCleaner.clean
+end
+
+After do
+  page.driver.restart if defined?(page.driver.restart)
+end
+
+Before('@selenium') do
+  Capybara.current_driver = :selenium
+end
+
+After('@selenium') do
+  Capybara.javascript_driver = :poltergeist
+  Capybara.use_default_driver
+end
